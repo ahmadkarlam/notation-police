db915cb61a83540c517b4dcebcbe2c83a1146ec7
Ahmad Karlam <ahmad.karlam@gmail.com>
2015-05-29 06:57:16 +0700

    Add main.pas

---
+++ main.pas
@@ -0,0 +1,241 @@
+program InfixToPostfix;
+
+type
+	Point = ^Data;
+	Data = Record
+		Character : Char;
+		Next : Point;
+	end;
+
+var
+	Contoh: String;
+	Contoh2: Char;
+
+function Validation(Infix : String) : Boolean;
+
+var
+	i, Wrong, FoundOperator, FoundDoubleOperand, FoundDoubleOperator: Integer;
+
+begin
+	Wrong := 0;
+	FoundOperator := 0;
+	FoundDoubleOperand := 0;
+	FoundDoubleOperator := 0;
+	for i := 1 to Length(Infix) do
+	begin
+		case Infix[i] of
+			'(', ')' : FoundOperator := FoundOperator + 1;
+			'+', '-', 
+			'*', '/', 
+			'^'       : begin
+							FoundOperator := FoundOperator + 1;
+							case Infix[i + 1] of
+								'+', '-', 
+								'*', '/', 
+								'^'       : FoundDoubleOperator := FoundDoubleOperator + 1;
+							end;
+						end;
+			'B', 'C', 
+			'D', 'E', 
+			'F', 'G', 
+			'H', 'I', 
+			'J', 'K', 
+			'L', 'M', 
+			'N', 'O', 
+			'P', 'Q', 
+			'R', 'S', 
+			'T', 'U', 
+			'V', 'W', 
+			'X', 'Y', 
+			'Z', 'A' : begin
+							case Infix[i + 1] of
+								'B', 'C', 
+								'D', 'E', 
+								'F', 'G', 
+								'H', 'I', 
+								'J', 'K', 
+								'L', 'M', 
+								'N', 'O', 
+								'P', 'Q', 
+								'R', 'S', 
+								'T', 'U', 
+								'V', 'W', 
+								'X', 'Y', 
+								'Z', 'A' : FoundDoubleOperand := FoundDoubleOperand + 1;
+							end;
+					   end;
+			else 
+				Wrong := Wrong + 1;
+		end;
+	end;
+	Validation := True;
+	If (Wrong > 0) Or (FoundOperator < 5) Or (FoundDoubleOperand > 0) Or (FoundDoubleOperator > 0) Then
+		Validation := False;
+end;
+
+function IsEmpty(Stack : Point): Boolean;
+begin
+	IsEmpty := False;
+	If (Stack = nil) Then
+		IsEmpty := True;
+end;
+
+function OneNode(Stack : Point): Boolean;
+begin
+	OneNode := False;
+	If (Stack^.Next = nil) Then
+		OneNode := True;
+end;
+
+procedure Initialize(var Stack : Point);
+
+begin
+	Stack := nil;
+end;
+
+procedure Push(var Stack : Point; Elemen : Char);
+
+var
+	Node: Point;
+
+begin
+	New(Node);
+	Node^.Character := Elemen;
+	Node^.Next := Stack;
+	Stack := Node;
+end;
+
+procedure Pop(var Stack : Point; var Elemen : Char);
+
+var
+	Node: Point;
+
+begin
+	Node := Stack;
+	Elemen := Node^.Character;
+	if (not OneNode(Stack)) then
+	begin
+		Stack := Stack^.Next;
+	end
+	else
+	begin
+		Stack := nil;
+	end;
+	Dispose(Node);
+end;
+
+procedure ConvertInfixToPostfix(Infix : String; var Postfix : String);
+
+var
+	i: Integer;
+	Stack: Point;
+	LastCharacter : Char;
+	P : String;
+
+begin
+	Initialize(Stack);
+	Push(Stack, '(');
+	Infix := Infix + ')';
+	P := '';
+	Writeln('Q : ',Infix);
+	for i := 1 to Length(Infix) do
+	begin
+		case Infix[i] of
+			'B', 'C', 
+			'D', 'E', 
+			'F', 'G', 
+			'H', 'I', 
+			'J', 'K', 
+			'L', 'M', 
+			'N', 'O', 
+			'P', 'Q', 
+			'R', 'S', 
+			'T', 'U', 
+			'V', 'W', 
+			'X', 'Y', 
+			'Z', 'A' : P := P + Infix[i];							{ OPERAND KETEMU }
+			'('      : Push(Stack, Infix[i]);						{ KURUNG BUKA KETEMU }
+			'+', '-', 
+			'*', '/', 
+			'^'       : begin 										{ OPERATOR KETEMU }
+							case Infix[i] of
+								'^': Push(Stack, Infix[i]);
+								'*',
+								'/': begin
+										while (Stack^.Character <> '(')	do
+										begin
+											case Stack^.Character of
+												'^',
+												'*',
+												'/' : begin
+															Pop(Stack, LastCharacter);
+															P := P + LastCharacter;
+														end;
+											end;
+										end;
+										Push(Stack, Infix[i]);
+									 end;
+								'+',
+								'-': begin
+										while (Stack^.Character <> '(')	do
+										begin
+											case Stack^.Character of
+												'^',
+												'*',
+												'/',
+												'+',
+												'-' : begin
+															Pop(Stack, LastCharacter);
+															P := P + LastCharacter;
+														end;
+											end;
+										end;
+										Push(Stack, Infix[i]);
+									 end;
+							end;
+						end;
+			')' : begin
+					while (Stack^.Character <> '(')	do
+					begin
+						Pop(Stack, LastCharacter);
+						P := P + LastCharacter;
+					end;
+					if (i <> Length(Infix)) Then
+						Pop(Stack, LastCharacter);
+				  end;
+		end;
+	end;
+	Postfix := P;
+	Writeln('P : ',Postfix);
+end;
+
+procedure InputInfix(var Infix : String);
+begin
+	Repeat
+		Write('Masukkan notasi infix : ');
+		Readln(Infix);
+	Until Validation(Infix);
+end;
+
+procedure Main();
+
+var
+	Infix, Postfix: String;
+
+begin
+	{ Input Infix }
+	InputInfix(Infix);
+	{ Convert Infix to Postfix }
+	ConvertInfixToPostfix(Infix, Postfix);
+	{ 
+		Memasukkan nilai pada setiap huruf di notasi infix 
+		
+	}
+	{ Huruf berubah menjadi angka pada notasi postfix }
+	{ Menghitung hasil dari notasi postfix }
+end;
+
+begin
+	Main();
+	Readln;
+end.
